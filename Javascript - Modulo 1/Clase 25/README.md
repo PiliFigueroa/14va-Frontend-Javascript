7e2c29708b38dc99295761dee457e46785479057
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2022-07-25 21:15:49 -0300

    clase 25 - variables

---
+++ Javascript - Modulo 1/Clase 25/README.md
@@ -0,0 +1,11 @@
+### Ejercicio 1:
+
+- Declarar variables con datos de un usuario (user, age, email)
+- Mostrar estos datos por consola
+- Mostrar el tipo de dato de cada variable por consola
+
+### Ejercicio 2:
+
+- Pedir al usuario sus datos personales (nombre, edad, pais, profesion)
+- Mostrar esos datos por consola usando concatenacion
+- Mostrar por consola la edad + 10
---
+++ Javascript - Modulo 1/Clase 25/js/script.js
@@ -0,0 +1,74 @@
+// NO TIPADO
+// CASE SENSITIVE
+// Variables y Tipos de datos
+// definir la variable
+
+// let nombre = "Pilar"
+// console.log("defino nombre:", nombre)
+// la uso
+
+// nombre = "Ramon"
+// console.log("cambio nombre:", nombre)
+
+// TIPOS DE DATOS PRIMITIVOS
+// let nombre = "Pilar" // string
+// let noticia = "River quedo fuera de la copa libertadores contra velez por un gol anulado por el VAR"
+// let edad = 25 // number
+// let pais = "Argentina"
+// let PI = 3.14 // number
+// let sabeManbejar = false // boolean
+// let juegaAlLoL = true // boolean
+// let noDefinido = undefined // undefined
+
+// Imprimir un texto por consola
+// concatenar: unir variables o informacion
+
+// console.log("Mi nombre es " + nombre + " y tengo " + edad + " anios " + "y soy de " + pais)
+
+// nombre = "Ivana"
+// edad = 49
+
+// template literals (backticks)
+// console.log(`Mi nombre es ${nombre} y tengo ${edad} anios y soy de ${pais}`)
+
+// // typeof
+
+// console.log(typeof nombre)
+// console.log(typeof edad)
+// console.log(typeof sabeManbejar)
+// console.log(typeof juegaAlLoL)
+// console.log(typeof noDefinido)
+
+// let nombre = prompt("Ingrese su nombre:")
+// let edad = prompt("Ingrese su edad:")
+// let pais = prompt("Ingrese su pais:")
+// alert("Usted no tiene acceso a esta pagina")
+// alert(nombre)
+// camelCase
+// let esMayorDeEdad = confirm("Es usted mayor de edad?")  // true o false
+
+// console.log(`Mi nombre es ${nombre} y tengo ${edad} anios y soy de ${pais}`)
+// console.log('Nuestro pais tiene una economia "estable"')
+
+/* Operadores aritmeticos */
+
+/*
+suma +
+resta -
+multiplicacion *
+division /
+modulo %
+*/
+
+// let numero = 5
+// let numero2 = 2
+
+let numero = prompt("Ingrese el primer numero:", 0)
+let numero2 = prompt("Ingrese el segundo numero:", 0)
+
+// parseItn() es un metodo que convierte un string a un numero entero
+console.log("Suma", parseInt(numero) + parseInt(numero2))
+console.log("Resta", numero - numero2)
+console.log("Producto", numero * numero2)
+console.log("Division", numero / numero2)
+console.log("Modulo", numero % numero2)

8aba84fda11dde7b84a14c7c4cf279fd74f00b89
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2022-08-10 21:04:00 -0300

    agrego ejercicios de los grupos

--- Javascript - Modulo 1/PracticaIntegradora/Grupo Principal - Practica Integradora/script.js
+++ Javascript - Modulo 1/PracticaIntegradora/Grupo Principal - Practica Integradora/script.js
@@ -2,28 +2,28 @@
 
 //UNO Crear una funcion que reciba por parametro un string y devuelva solo la primer letra de dicho string.
 
-/*
+
 const primerLetra = (texto) => {
  return texto.charAt(0)
 }
 
-console.log(primerLetra("Lucia"))
-*/
+// console.log(primerLetra("Lucia"))
+
 
 //DOS Crear una funcion que reciba por parametro un string y devuelva la ultima letra de dicho string.
 
-/*
-const ultimaLetra = (texto) => {
-    const resultado = texto.length
-    return texto.charAt(resultado-1)
-   }
+
+// const ultimaLetra = (texto) => {
+//     const resultado = texto.length
+//     return texto.charAt(resultado-1)
+//    }
 
    
-   console.log(ultimaLetra("Lucia"))
-*/
+//    console.log(ultimaLetra("Lucia"))
+
 
 //TRES Crear una funcion que reciba tres parametros number y que permita sumar los primeros dos, o los tres valores en caso de recibir los tres. Pero si solo recibe dos valores, que sume solo esos dos.
-/*
+
 const suma = (num1, num2, num3) => {
     if (typeof num1 === 'number' && typeof num2 === 'number' && typeof num3 === 'number') {
         return num1 + num2 + num3
@@ -33,23 +33,23 @@ const suma = (num1, num2, num3) => {
 }
 
 console.log(suma(1, 2))
-*/
+
 //CUATRO Investigar que metodo podemos utilizar para sacar la raiz cuadrada de un numero.Una vez investigado, crear una funcion que devuelva el resultado.
-/*
+
 const raizCuadrada = (numero) => {
     return Math.sqrt(numero)
 }
 console.log(raizCuadrada(16))
-*/
+
 
 //CINCO Investigar que metodo podemos utilizar para sacar el cuadrado de un numero. Una vez investigado, crear una funcion que devuelva el resultado.
-/*
+
 const cuadrado = (num1, num2) => {
     return Math.pow(num1, num2)
 }
 
 console.log(cuadrado(2, 2))
-*/
+
 
 //SEIS Crear una función gritar que tome como argumento un string y devuelva el mismo string con un signo de exclamación al principio y al final del mismo.
 /*
@@ -95,15 +95,15 @@ console.log(calcularFPS(2, 3))
 
 //DIEZ Crear una función sumarPorcentaje que tome como argumentos un número y un porcentaje y devuelva la suma de dicho número con la de 
 //su porcentaje. Usar la función calcularPorcentaje para obtener el porcentaje a sumar.
-/*
+
 const sumarPorcentaje = (numero, porcentaje) => {
-    return (numero +(numero/100*porcentaje))
+    return numero + (numero / 100 * porcentaje)
 }
 
 console.log(sumarPorcentaje(100, 15))
 console.log(sumarPorcentaje(10, 50))
 console.log(sumarPorcentaje(200, 10))
-*/
+
 
 //ONCE //Crear una función restarPorcentaje que tome como argumentos un número y un porcentaje 
 //y devuelva la resta de dicho número con la de su porcentaje. Usar la función calcularPorcentaje para obtener el porcentaje a restar.
@@ -127,7 +127,7 @@ const generarEmail = (usuario, dominio) => {
 console.log(generarEmail("lucia", "gmail"))
 */
 //TRECE Crear una funcion que reciba dos strings y devuelva el numero de caracteres del string mas largo.
-/*
+
 const masLargo = (palabra1, palabra2) => {
     if (palabra1.length > palabra2.length){
         return palabra1.length
@@ -137,12 +137,12 @@ const masLargo = (palabra1, palabra2) => {
 }
 console.log(masLargo("amor", "amistad"))
 console.log(masLargo("amistad", "amor"))
-*/
+
 
 
 //CATORCE Crear una función aceptaDeposito que tome como argumento un número monto y
 // devuelva true si el monto es divisible por 10 o false si no lo es.
-/*
+
 const aceptaDeposito = (monto) => {
     if (monto % 10 === 0) {
         return true
@@ -155,11 +155,11 @@ console.log(aceptaDeposito(440))
 console.log(aceptaDeposito(123))
 console.log(aceptaDeposito(500.50))
 console.log(aceptaDeposito(1000))
-*/
+
 
 //QUINCE Crear una función esUltimoCaracter que tome como argumentos una palabra y un caracter y 
 //devuelva true si la palabra termina con el caracter o false de lo contrario.
-/*
+
 const esUltimoCaracter = (texto, caracter) => {
     const ultimoCaracter = texto.charAt(texto.length-1)
 
@@ -172,7 +172,7 @@ const esUltimoCaracter = (texto, caracter) => {
 
 console.log(esUltimoCaracter("lovelace", "e"))
 console.log(esUltimoCaracter("lovelace", "f"))
-*/
+
 
 //DIECISEIS Crear una función esValida que tome como argumento una contrasenia (string) y 
 //devuelva true si tiene 8 caracteres o más o false si tienen menos de 8 caracteres.
@@ -221,7 +221,7 @@ console.log(capitalizar("hola"))
 
 //DIECINUEVE //Crear una función obtenerDuracionEnSegundos que tome como argumento 
 //un string duracion con el formato (mm:ss) y la cantidad de segundos totales de la duración
-/*
+
 const obtenerDuracionEnSegundos = (hora) => {
     const minutos = (parseInt(hora.slice(0, 2))*60)
     const segundos = (parseInt(hora.slice(3, 5)))
@@ -229,7 +229,7 @@ const obtenerDuracionEnSegundos = (hora) => {
 }
 
 console.log(obtenerDuracionEnSegundos("10:42"))
-*/
+
 
 //VEINTE // Crear una funcion que reciba por parametro un string de saludo y 
 //devuelva true o false si el string incluye la palabra "Hola". Ojo las mayusculas.
@@ -261,7 +261,7 @@ console.log(contrasenia("chau", "hola"))
 
 //VEINTITRES //Crear una función esFraccionMayorAUno que tome como argumento un string fraccion (en el formato 'numerador/denominador')
 // y devuelva true si dicha fracción es mayor a 1 o false de lo contrario.
-/*
+
 const esFraccionMayorAUno = (fraccion) => {
     const numerador = parseInt(fraccion.slice(0, 1))
     const denominador = parseInt(fraccion.slice(2, 3))
@@ -272,10 +272,10 @@ const esFraccionMayorAUno = (fraccion) => {
     }
 }
 
-console.log(esFraccionMayorAUno("1/2"))
-console.log(esFraccionMayorAUno("2/2"))
-console.log(esFraccionMayorAUno("4/2"))
-*/
+console.log(esFraccionMayorAUno("1/2")) // 0.5
+console.log(esFraccionMayorAUno("2/2")) // 1
+console.log(esFraccionMayorAUno("4/2")) // 2
+
 
 //VEINTICUATRO // Crear una función esPuenteSeguro que tome como string un puente que consista en caracteres numerales y espacios y devuelva true si el puente está cortado (tiene espacios) o false si está entero y es seguro atravesar.
 /*
@@ -294,7 +294,7 @@ console.log(esPuenteSeguro("#########"))
 //VEINTICINCO //Crear una función obtenerExtension que tome como argumento un string archivo con el 
 //formato 'nombre.extension' y devuelva la extensión del archivo. 
 //Investigar la funcion nativa split() y pensar si nos ayuda para este problema.
-/*
+
 const obtenerExtension = (archivo) => {
     return archivo.split(".")[1]
 }
@@ -302,4 +302,3 @@ const obtenerExtension = (archivo) => {
 console.log(obtenerExtension("index.html"))
 console.log(obtenerExtension("index.js"))
 console.log(obtenerExtension("index.jpg"))
-*/

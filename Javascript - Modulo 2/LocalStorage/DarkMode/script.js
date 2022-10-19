cd4abf30caf44d9421928a01b361c60f0befd367
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2022-10-19 19:56:11 -0300

    agrego clase localstorage

---
+++ Javascript - Modulo 2/LocalStorage/DarkMode/script.js
@@ -0,0 +1,65 @@
+const body = document.querySelector("body")
+const btnCambiarModo = document.querySelector("#cambiar-modo")
+const textoModo = document.querySelector("#nombre-modo")
+
+/* Cuando el usuario entra a mi pagina por primera vez, no hay nada en el localstorage, por lo tanto
+entramos en el "else" de mi if y le seteamos el objeto "modo", lo pasamos a string y lo enviamos al
+localstorage del usuario.
+La proxima vez que el usuario entre a mi pagina, si tendra guardado el objeto modo en mi localstorage, por lo tanto
+ahora si va a entrar en el if ya que da true, y daremos los estilos segun que es lo que tiene guardado.*/
+
+if (localStorage.getItem('modo')) {
+    // obtengo lo que tiene guardado el usuario
+    const preferenciaModoJSON = localStorage.getItem('modo')
+    // lo parseo
+    const preferenciaModo = JSON.parse(preferenciaModoJSON)
+
+    // pregunto si lo que esta guardado es modo oscuro o claro, y seteo los estilos en base a eso
+    if (preferenciaModo.modo === 'oscuro') {
+        body.classList.add("modo-oscuro")
+        textoModo.textContent = "claro"
+    } else {
+        body.classList.remove("modo-oscuro")
+        textoModo.textContent = "oscuro"
+    }
+} else {
+    const modo = {
+        modo: "claro"
+    }
+    const preferenciaModo = JSON.stringify(modo)
+    localStorage.setItem('modo', preferenciaModo)
+}
+
+/* Inicialmente el usuario tendra en el localstorage el objeto modo seteado en "claro" y a su vez
+nuestra pagina tambien esta en modo claro, con el texto del span en "claro", por lo tanto, al dar click
+en mi boton y ejecutar este evento, entramos en el if ya que da true y cambiamos el texto del span a "oscuro",
+y a su vez mantenemos el objeto modo seteado en "claro" dentro del localstorage del usuario.
+Cuando el usuario vuelva a dar click, entrara en el "else" de mi if ya que ahora el texto del span dice "oscuro" por lo tanto
+ahora invertimos la logica, cambiando el texto del span a "claro" y enviando al localstorage del usuario el objeto modo pero
+seteado en oscuro. */
+
+btnCambiarModo.onclick = () => {
+    // Primero realizamos el toggle de la class modo-oscuro al body para cambiar la jerarquia de mis estilos
+    body.classList.toggle("modo-oscuro")
+    if (textoModo.textContent == "claro") {
+        textoModo.textContent = "oscuro"
+        // objeto
+        const modo = {
+            modo: "claro"
+        }
+        // convertirlo a json
+        const modoAJSON = JSON.stringify(modo)
+        // guardarlo en local storage
+        localStorage.setItem('modo', modoAJSON)
+    } else {
+        textoModo.textContent = "claro"
+        // objeto
+        const modo = {
+            modo: "oscuro"
+        }
+        // convertirlo a json
+        const modoAJSON = JSON.stringify(modo)
+        // guardarlo en local storage
+        localStorage.setItem('modo', modoAJSON)
+    }
+}
--- Javascript - Modulo 2/LocalStorage/script.js
+++ Javascript - Modulo 2/LocalStorage/script.js
@@ -1,1 +1,40 @@
-// Localstorage
+// LocalStorage
+
+const user = {
+    name: "Pilar Figueroa",
+    age: 25
+}
+
+// console.log(user)
+// JSON stringify
+// console.log(JSON.stringify(user))
+
+//const userJSON = JSON.stringify(user)
+
+//localStorage.setItem("user", JSON.stringify(user))
+
+// JSON parse
+// console.log(localStorage.getItem("clave"))
+
+// const userLocalStorage = JSON.parse(localStorage.getItem("clave"))
+
+// userLocalStorage.name = "Aldana Sorni"
+//console.log(userLocalStorage)
+//localStorage.setItem("clave", JSON.stringify(userLocalStorage))
+
+// localStorage.removeItem("clave")
+
+if (localStorage.getItem("user")) {
+    const userLocalStorage = JSON.parse(localStorage.getItem("user"))
+    userLocalStorage.age = 26
+    document.write(userLocalStorage.age)
+    localStorage.setItem("user", JSON.stringify(userLocalStorage))
+} else {
+    localStorage.setItem("user", JSON.stringify(user))
+}
+
+// setItem() recibe una key que es un nombre de referencia y el dato que queremos almacenar en el storage, y este dato debe ser en formato JSON, por eso usamos JSON.stringify. A su vez, si queremos modificar un dato existente, lo enviamos con este mismo metodo usando la misma key.
+
+// getItem() nos permite traer informacion guardada en el localstorage, mediante la key de referencia
+
+// removeItem() nos permite eliminar informacion guardada en el localstorage, mediante la key de referencia

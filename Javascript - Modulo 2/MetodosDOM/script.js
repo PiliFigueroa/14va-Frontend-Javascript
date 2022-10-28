582f70914dd3fb2efb5fbd1d7927c63c1a4de7d1
Pilar Figueroa <mpilarfigueroa.96@gmail.com>
2022-10-28 19:50:26 -0300

    agrego ejemplo de clase alternativa con ternario

--- Javascript - Modulo 2/MetodosDOM/script.js
+++ Javascript - Modulo 2/MetodosDOM/script.js
@@ -4,35 +4,40 @@ const products = [
         name: "Backpack",
         price: 11000,
         image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        category: "accesories"
+        category: "accesories",
+        free_shipping: true
     },
     {
         id: "2h6j-6k7j",
         name: "Ring",
         price: 25000,
         image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
-        category: "jewelery"
+        category: "jewelery",
+        free_shipping: false
     },
     {
         id: "1u8d-8j4b",
         name: "ROM Memory",
         price: 9990,
         image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
-        category: "electornics"
+        category: "electornics",
+        free_shipping: true
     },
     {
         id: "0d3j-4j7k",
         name: "Rock and roll badass jacket",
         price: 15250,
         image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
-        category: "womens"
+        category: "womens",
+        free_shipping: false
     },
     {
         id: "9l2d-4n6u",
         name: "Winter Jacket",
         price: 16090,
         image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
-        category: "womens"
+        category: "womens",
+        free_shipping: true
     },
 ]
 
@@ -62,14 +67,16 @@ const generateCards = (products) => {
 */
 
 const generateCards = () => {
-    for (const { id, name, price, image, category } of products) {
+    for (const { id, name, price, image, category, free_shipping } of products) {
         const div = document.createElement("div")
         div.classList.add("card")
+        div.setAttribute("style", "width: 300px;")
         div.innerHTML = `
             <img src="${image}" class="card-img-top" alt="${name}">
             <div class="card-body">
                 <h5 class="card-title">${name}<span class="badge bg-danger capitalize">${category}</span></h5>
                 <p class="card-text">$${price}</p>
+                ${free_shipping ? '<span class="badge bg-warning capitalize">Envio gratis! :)</span>' : ""}               
                 <button class="btn btn-success btn-edit" data-id="${id}">Edit</button>
             </div>
         `
